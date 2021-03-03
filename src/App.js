ccd4ec05e4a437a5aff0f582a109fcce1937bf1b
suzukitoshinari <ay658mymt@gmail.com>
2021-03-03 22:55:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
+import CanvasDraw from "react-canvas-draw";
 import './App.css';
+import { SketchPicker } from "react-color";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+  const canvasRef = useRef(null);
+  const [color, setColor] = useState('black');
+  const [thick, setThick] = useState('2');
+
+  const Save = () => {
+    const data = canvasRef.current.getSaveData();
+    console.log(data);
+  }
+
+  const Clear = () => {
+    canvasRef.current.clear();
+  }
+
+  const Undo = () => {
+    canvasRef.current.undo();
+  }
+
+  return ( 
+    <>
+      <header className='header'>
+        React-Canvas-Draw
       </header>
-    </div>
+      <div className='container'>
+        <div className='btn'>
+          <button onClick={Save}>保存</button>
+          <button onClick={Clear}>削除</button>
+          <button onClick={Undo}>前に戻る</button>
+          <label className='thick'>太く</label>
+          <input type='number' value={thick} onChange={e => setThick(parseInt(e.target.value, 10))}></input>
+          <SketchPicker
+          color={color}
+          onChangeComplete={ (color) => { setColor(color.hex) } }
+          />
+        </div>
+        <CanvasDraw 
+          brushRadius={thick}
+          hideGrid={true}
+          style={{border: '1px solid black'}}
+          canvasHeight={600}
+          canvasWidth={600}
+          ref={canvasRef}
+          lazyRadius={1}
+          brushColor={color}
+        />
+      </div>
+    </>
   );
 }
 
